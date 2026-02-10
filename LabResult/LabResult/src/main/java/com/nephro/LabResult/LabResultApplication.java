331251fdd44cd6f23854599fb33a57d5ad6264f1
eyakhadhraoui <eya.khadhraoui@esprit.tn>
2026-02-10 20:41:50 +0100

    add microservices structure

---
+++ LabResult/LabResult/src/main/java/com/nephro/LabResult/LabResultApplication.java
@@ -0,0 +1,13 @@
+package com.nephro.LabResult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabResultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabResultApplication.class, args);
+	}
+
+}
---
+++ LabResult/LabResult/src/test/java/com/nephro/LabResult/LabResultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nephro.LabResult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
