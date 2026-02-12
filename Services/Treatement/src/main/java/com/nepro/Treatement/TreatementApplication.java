9821592f9afd955183ab715ce43303a505cc4741
loujainrouached <loujein.rouached@esprit.tn>
2026-02-12 09:16:40 +0100

    Architecture

---
+++ Services/Treatement/src/main/java/com/nepro/Treatement/TreatementApplication.java
@@ -0,0 +1,13 @@
+package com.nepro.Treatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatementApplication.class, args);
+	}
+
+}
---
+++ Services/Treatement/src/test/java/com/nepro/Treatement/TreatementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepro.Treatement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
