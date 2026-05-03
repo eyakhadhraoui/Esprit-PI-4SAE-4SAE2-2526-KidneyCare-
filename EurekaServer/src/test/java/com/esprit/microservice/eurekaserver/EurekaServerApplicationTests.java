db5475a7f1f55f836f0d76ec0c4c86dee6de5763
loujainrouached <loujainrouached@gmail.com>
2026-05-03 13:03:29 +0100

    finale pipeline Kidneeeyyy14

--- EurekaServer/src/test/java/com/esprit/microservice/eurekaserver/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/esprit/microservice/eurekaserver/EurekaServerApplicationTests.java
@@ -7,10 +7,15 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
+        "eureka.client.enabled=false",
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false",
+        "spring.cloud.config.enabled=false",
         "spring.autoconfigure.exclude=" +
             "org.springframework.cloud.netflix.eureka.server.EurekaServerAutoConfiguration," +
             "org.springframework.cloud.netflix.eureka.server.EurekaServerJerseyClientAutoConfiguration," +
-            "org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration"
+            "org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration," +
+            "org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration"
     }
 )
 @ActiveProfiles("test")
